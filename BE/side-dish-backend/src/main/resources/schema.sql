2d429399d3ce8b8f3fa0c2e75c0ca9335bddd039
Sally Oh <min27604@hotmail.com>
2021-04-28 23:35:28 +0900

    fix: 카테고리를 통해서 아이템을 못 찾는 버그 해결
    Long 이라 DB에 BIGINT로 들어가야하는데 INT로 되어있어서 에러가 떴다

--- BE/side-dish-backend/src/main/resources/schema.sql
+++ BE/side-dish-backend/src/main/resources/schema.sql
@@ -1,11 +1,11 @@
 CREATE TABLE IF NOT EXISTS `sidedish`.`category` (
-                                                     `id` INT PRIMARY KEY AUTO_INCREMENT,
+                                                     `id` BIGINT PRIMARY KEY AUTO_INCREMENT,
                                                      `name` VARCHAR(45) NOT NULL,
                                                      `best` TINYINT(1)
 );
 
 CREATE TABLE IF NOT EXISTS `sidedish`.`item` (
-		`id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+		`id` BIGINT PRIMARY KEY AUTO_INCREMENT,
 		`title` VARCHAR(45) NOT NULL,
 		`description` VARCHAR(200) NOT NULL,
 		`normal_price` INT NOT NULL,
@@ -13,6 +13,5 @@ CREATE TABLE IF NOT EXISTS `sidedish`.`item` (
 		`badges` VARCHAR(45) NULL,
 		`delivery_types` VARCHAR(200) NULL,
 		`images` VARCHAR(2000) NULL,
-		`category_name` VARCHAR(45),
 		`category` INT REFERENCES category(`id`)
 );
